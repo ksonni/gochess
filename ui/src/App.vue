ff8b26a41cdd19800e8fd54e92e8c1063721fd7c
Keshav Sonni <keshav.vm97@gmail.com>
2025-08-22 19:38:01 +0100

    ui: barebones chessboard

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,8 +1,22 @@
-<script setup></script>
+<script setup>
+
+import { TheChessboard } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
+
+</script>
 
 <template>
-  <h1>Chess</h1>
-  <p>Chess is a beautiful game</p>
+  <div class="board-container">
+    <TheChessboard />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.board-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
+
