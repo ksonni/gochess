dd502fd9cc010bfaee04e7b11374bafe1afcdba9
Keshav Sonni <keshav.vm97@gmail.com>
2025-08-22 18:08:35 +0100

    game: misc controller code cleanup

--- app/auth/router.go
+++ app/auth/router.go
@@ -4,10 +4,12 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+var c = NewController()
+
 func RegisterPublicRoutes(r chi.Router) {
-	r.Post("/auth/register", registrationHandler)
+	r.Post("/auth/register", c.registrationHandler)
 }
 
 func RegisterRoutes(r chi.Router) {
-	r.Get("/auth/me", meHandler)
+	r.Get("/auth/me", c.meHandler)
 }
